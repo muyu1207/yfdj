36f95fe88a4478ffc2091b87de8a0f183d9aab7c
muyu1207 <zx222@qq.com>
2016-02-02 13:41:54 +0800

    控制器简单完成

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -5,21 +5,29 @@ class IndexController extends Controller {
     public function index(){
         $this->display();
     }
-    
+//    专题列表
     public function zhuanti(){
         $this->display();
     }
-    
+//    专题详情
     public function post(){
         $this->display();
     }
-    
+//    小吃街列表
     public function xiaochi(){
         $this->display();
     }
     
 //    商品详情
     public function product(){
+        $this->display();
+    }
+//    地方馆列表
+    public function place(){
+        $this->display();
+    }
+//    地方馆详情
+    public function road(){
         $this->display();
     }
 }
